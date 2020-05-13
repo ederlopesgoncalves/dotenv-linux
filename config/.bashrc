59441a9123aab06bc977c7a6354245eb1a37dabe
Eder Gonçalves <ederlopesgoncalves@gmail.com>
2020-05-12 22:57:45 -0300

    Adding first default config files

---
+++ config/.bashrc
@@ -0,0 +1,11 @@
+# Add at end of bashrc file - Bash Personalization ======================================
+
+# if .git-prompt.sh exists, set options and execute it
+if [ -f ~/.git-prompt.sh ]; then
+  . ~/.git-prompt.sh
+fi
+
+#========================================================================================
+
+
+
---
+++ config/.git-prompt.sh
@@ -0,0 +1,42 @@
+# Git ========================================================================================
+# store colors
+MAGENTA="\[\033[0;35m\]"
+YELLOW="\[\033[01;33m\]"
+BLUE="\[\033[00;34m\]"
+LIGHT_GRAY="\[\033[0;37m\]"
+CYAN="\[\033[0;36m\]"
+GREEN="\[\033[1;32m\]"
+RED="\[\033[0;31m\]"
+VIOLET='\[\033[01;35m\]'
+ 
+function color_my_prompt {
+  local __user_and_host="$GREEN\u@\h"
+  local __cur_location="$BLUE\W"           # capital 'W': current directory, small 'w': full file path
+  local __git_branch_color="$GREEN"
+  local __prompt_tail="$VIOLET$"
+  local __user_input_color="$GREEN"
+  local __git_branch=$(__git_ps1); 
+  
+  # colour branch name depending on state
+  if [[ "${__git_branch}" =~ "*" ]]; then     # if repository is dirty
+      __git_branch_color="$RED"
+  elif [[ "${__git_branch}" =~ "$" ]]; then   # if there is something stashed
+      __git_branch_color="$YELLOW"
+  elif [[ "${__git_branch}" =~ "%" ]]; then   # if there are only untracked files
+      __git_branch_color="$LIGHT_GRAY"
+  elif [[ "${__git_branch}" =~ "+" ]]; then   # if there are staged files
+      __git_branch_color="$CYAN"
+  fi
+   
+  # Build the PS1 (Prompt String)
+  PS1="$__user_and_host $__cur_location$__git_branch_color$__git_branch $__prompt_tail$__user_input_color "
+}
+ 
+# configure PROMPT_COMMAND which is executed each time before PS1
+export PROMPT_COMMAND=color_my_prompt
+
+# Original => \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$
+
+
+
+
